d7c9509dafb3af6f0b1a240e59fb942deefa1b8c
Yashawant <yashawant.nita@gmil.com>
2019-01-02 20:53:20 +0530

    Modified

--- Test/MemberTest.java
+++ Test/MemberTest.java
@@ -7,6 +7,7 @@ public class MemberTest {
     Member obj ;
     @Before
     public void setUp() {
+
         obj = new Member();
     }
 
@@ -19,21 +20,27 @@ public class MemberTest {
     @Test
     public void testName()
     {
-
+        obj.setName("Rahul");
+        String actual = obj.getName();
+        String expected = "Rahul"
         assertEquals(expected,actual);
     }
 
     @Test
     public void testAge()
     {
-
+        obj.setAge(22);
+        int actual = obj.getAge();
+        int expected = 22;
         assertEquals(expected,actual);
     }
 
     @Test
     public void testSalary()
     {
-
+        obj.setSalary(10000);
+        int actual = getSalary();
+        int expected = 10000;
         assertEquals(expected,actual);
     }
 
