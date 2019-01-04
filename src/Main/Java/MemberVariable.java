ac92570130c7e812f29f607acfdb2d54196a80d3
yasawant.k.rosan <yasawant.k.rosan@boeing.com>
2019-01-04 15:29:40 +0530

    Modified

--- src/Main/Java/MemberVariable.java
+++ src/Main/Java/MemberVariable.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class MemberVariable {
     public static void main(String args[]){
-        Member obj = new Member();
+        Member member = new Member();
         System.out.println("Enter the name: ");
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
-        obj.setName(name);
+        member.setName(name);
         System.out.println("Enter the age: ");
         int age = sc.nextInt();
-        obj.setAge(age);
+        member.setAge(age);
         System.out.println("Enter the salary: ");
         double salary = sc.nextDouble();
-        obj.setSalary(salary);
-        System.out.println("Name: " + obj.getName());
-        System.out.println("Age: "+obj.getAge());
-        System.out.println("Salary: "+obj.getSalary());
+        member.setSalary(salary);
+        System.out.println("Name: " + member.getName());
+        System.out.println("Age: "+member.getAge());
+        System.out.println("Salary: "+member.getSalary());
     }
 }
